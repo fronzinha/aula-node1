104524f47270661083ba056fd8e7719d99f84eb1
fronzinha <joaofronza14@gmail.com>
2023-06-29 19:29:46 -0300

    aula 05

---
+++ aula05/correcao-exercicio/departamentos.js
@@ -0,0 +1,22 @@
+[
+    {
+        "id_departamento": 1,
+        "sigla": "DP",
+        "nome": "Departamento Pessoal"
+    },
+    {
+        "id_departamento": 125,
+        "sigla": "RH",
+        "nome": "Recursos Humanos"
+    },
+    {
+        "id_departamento": 118,
+        "sigla": "TI",
+        "nome": "Tecnologia da Informação"
+    },
+    {
+        "id_departamento": 119,
+        "sigla": "SRE",
+        "nome": "Site Reliability Engineering"
+    },
+]
---
+++ aula05/correcao-exercicio/index.js
@@ -0,0 +1,6 @@
+import express from 'express'
+
+const app = express()
+
+
+app.listen(3033, () => console.log('Servidor online'))
