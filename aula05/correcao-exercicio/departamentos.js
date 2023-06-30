d733c9f468969747f4ffc77f1ddff246b062d0ae
fronzinha <joaofronza14@gmail.com>
2023-06-29 21:23:36 -0300

    fim exercicio

--- aula05/correcao-exercicio/departamentos.js
+++ aula05/correcao-exercicio/departamentos.js
@@ -1,22 +1,33 @@
-[
+const departamentos = [
     {
-        "id_departamento": 1,
-        "sigla": "DP",
+        id_departamento: 1,
+        sigla: "DP",
         "nome": "Departamento Pessoal"
     },
     {
-        "id_departamento": 125,
-        "sigla": "RH",
-        "nome": "Recursos Humanos"
+        id_departamento: 2,
+        sigla: "RH",
+        nome: "Recursos Humanos"
     },
     {
-        "id_departamento": 118,
-        "sigla": "TI",
-        "nome": "Tecnologia da Informação"
+        id_departamento: 3,
+        sigla: "TI",
+        nome: "Tecnologia da Informação"
     },
     {
-        "id_departamento": 119,
-        "sigla": "SRE",
-        "nome": "Site Reliability Engineering"
+        id_departamento: 4,
+        sigla: "SRE",
+        nome: "Site Reliability Engineering"
     },
 ]
+
+export const getDepartamentos = () => {
+    return departamentos
+}
+
+export const getDepartamento = (id_departamento) => {
+    return departamentos.filter((item) => {
+        return item.id_departamento == id_departamento
+    })
+}
+
--- aula05/correcao-exercicio/index.js
+++ aula05/correcao-exercicio/index.js
@@ -1,6 +1,23 @@
 import express from 'express'
+import { getDepartamentos, getDepartamento } from './departamentos.js'
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send('Oi Dev')
+})
+
+app.get('/departamento', (req, res) => {
+
+  const result = getDepartamentos()
+  res.json(result)
+
+})
+
+app.get('/departamento/:id_departamento', (req, res) => {
+  const { id_departamento } = req.params
+  const result = getDepartamento(id_departamento)
+  res.json(result)
+})
 
 app.listen(3033, () => console.log('Servidor online'))
