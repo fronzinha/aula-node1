e2c38e4aad685393112037b6f44aa8cc26779633
fronzinha <joaofronza14@gmail.com>
2023-07-24 21:30:50 -0300

    aula final

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import departamentosRoutes from './src/routes/departamentosRoutes.js'
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded( { extended: true } ))
+
+departamentosRoutes(app)
 
 app.get('/', (req, res) => {
-    res.send('Oi dev')
+  res.send('Welcome to API')
 })
 
-app.listen(3033, () => {
-    console.log('Servidor Online')
+app.listen(80, () => {
+  console.log('Servidor sendo executado...')
 })
+
+
